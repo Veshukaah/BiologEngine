095194e17018f6a65d9aa1239d7a6fa4cb99cdab
Наденька <vesukaah@gmail.com>
2022-12-15 18:00:05 +0500

    PreIntiliaze документация

--- BiologEngine/PreIntiliaze.cs
+++ BiologEngine/PreIntiliaze.cs
@@ -7,17 +7,17 @@ using System.Threading.Tasks;
 namespace BiologEngine
 {
     /// <summary>
-    /// Класс для обьявления параметров до Ин
+    /// Класс для обьявления параметров до инцилизации.
     /// </summary>
     public static class PreIntiliaze
     {
         internal static GameObject[] GameObjects = new GameObject[0];
 
         /// <summary>
-        /// 
+        /// Добовляет игровой объект.
         /// </summary>
-        /// <param name="gameObjects"></param>
-        public static void SetGameObject(GameObject gameObjects)
+        /// <param name="gameObjects"> Ссылка на игровой объект.</param>
+        public static void AddGameObject(GameObject gameObjects)
         {
             GameObjects = GameObjects.Concat(new GameObject[] { gameObjects }).ToArray();
         }
