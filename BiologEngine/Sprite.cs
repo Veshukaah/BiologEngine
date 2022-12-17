2df385a0043e331edd812a29f0c4d2f9e14f4726
Наденька <vesukaah@gmail.com>
2022-12-17 11:09:21 +0500

    Сделал документцаю спрайта

--- BiologEngine/Sprite.cs
+++ BiologEngine/Sprite.cs
@@ -4,23 +4,23 @@ using System.Drawing;
 namespace BiologEngine
 {
     /// <summary>
-    /// 
+    /// Класс спрайта.
     /// </summary>
     public class Sprite
     {
         /// <summary>
-        /// 
+        /// Ссылка на движок.
         /// </summary>
         public Engine engine;
         /// <summary>
-        /// 
+        /// Экземпляр кисти.
         /// </summary>
         public Brush brush;
 
         /// <summary>
-        /// 
+        /// Конструктор спрайта.
         /// </summary>
-        /// <param name="brush"></param>
+        /// <param name="brush">Экземпляр кисти.</param>
         public Sprite( Brush brush)
         {
             this.brush = brush;
