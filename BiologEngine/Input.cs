89699c37ac232315606bc22de41fae575c19fd5f
Наденька <vesukaah@gmail.com>
2022-12-18 13:02:01 +0500

    Сделал Input

---
+++ BiologEngine/Input.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BiologEngine
+{
+    /// <summary>
+    /// Класс отвечающий за нажатие клавиш.
+    /// </summary>
+    public static class Input
+    {
+        /// <summary>
+        /// Проверка нажата ли клавиша.
+        /// </summary>
+        /// <param name="key">Клавиша для проверки.</param>
+        /// <returns>Нажата ли клавиша.</returns>
+        public static bool IsKeyDown(Key key)
+        {
+            if(Keyboard.IsKeyDown(key)) return true;
+            return false;
+        }
+    }
+}
