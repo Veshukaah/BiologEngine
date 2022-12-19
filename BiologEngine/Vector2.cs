6d5f7fd7a64decc437dc0742586f9ad09df33d8a
Наденька <vesukaah@gmail.com>
2022-12-19 14:00:02 +0500

    Доделал документацию

--- BiologEngine/Vector2.cs
+++ BiologEngine/Vector2.cs
@@ -52,10 +52,22 @@ namespace BiologEngine
         {
             return new Vector2(vector1.x - vector2.x, vector1.y - vector2.y);
         }
+        /// <summary>
+        /// Сравнивает вектора.
+        /// </summary>
+        /// <param name="vector1">Первый вектор.</param>
+        /// <param name="vector2">Второй вектор.</param>
+        /// <returns>True если первый вектор меньше второво.</returns>
         public static bool operator <(Vector2 vector1, Vector2 vector2)
         {
             return (vector1.x < vector2.x) & (vector1.y < vector2.y);
         }
+        /// <summary>
+        /// Сравнивает вектора.
+        /// </summary>
+        /// <param name="vector1">Первый вектор.</param>
+        /// <param name="vector2">Второй вектор.</param>
+        /// <returns>True если первый вектор больше второво.</returns>
         public static bool operator >(Vector2 vector1, Vector2 vector2)
         {
             return (vector1.x > vector2.x) & (vector1.y > vector2.y);
